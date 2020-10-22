8889a37dc6a367ab0085f380a25b1141d0484a96
AbrahamMugerwa <abraham.mugerwa@gmail.com>
2020-10-22 16:55:43 -0600

    lab2

---
+++ showorhide.js
@@ -0,0 +1,32 @@
+
+  function myFunction(){
+    var x = document.getElementById("loginPage");
+    var y = document.getElementById("signUpPage");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    y.style.display = "none";
+  } else {
+    x.style.display = "none";
+    y.style.display = "block"
+  }
+}
+
+function myFunction2(){
+  var x = document.getElementById("signUpPage");
+  var y = document.getElementById("loginPage");
+if (x.style.display === "none") {
+  x.style.display = "block";
+  y.style.display = "none";
+
+} else {
+  x.style.display = "none";
+  y.style.display = "block";
+  
+}
+}
+  
+function myFunction3()
+{
+  
+}
+    
